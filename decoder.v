b4d9ce229a3c12e96a83e11c9b9eef7974811d92
Ali <ali@Alis-MacBook-Pro.local>
2018-06-05 16:21:33 +0430

    make decoder compilable

--- decoder.v
+++ decoder.v
@@ -1,23 +1,29 @@
 module decoder(
-	input clk,
-	input start,
-	input[width_in-1:0] reg instruction_in,
-	output[width_out-1:0] reg instruction_out,
-	output reg ready
-)
-parameter byte = 8;
-parameter width_in = 4 * byte;
-parameter width_out = 4 * byte;
+	clk,
+	start,
+	instruction_in,
+	instruction_out,
+	ready
+);
+	parameter byte = 8;
+	parameter width_in = 4 * byte;
+	parameter width_out = 4 * byte;
 
-always @(posedge clk)
-begin
-	case(instruction_in[width_in-1:width-1-byte])
-		8'b00000000:
-		begin
-		end
-		default:
-		begin
-		end
-	endcase
-end
+	input clk;
+	input start;
+	input [width_in-1:0] instruction_in;
+	output reg [width_out-1:0] instruction_out;
+	output reg ready;
+
+	always @(posedge clk)
+	begin
+		case(instruction_in[width_in-1:width_in-1-byte])
+			8'b00000000:
+			begin
+			end
+			default:
+			begin
+			end
+		endcase
+	end
 endmodule 
