1c0f8042f0e6dabb97ec05eb7b61696113eb7229
rezazeiny <zeiny@ce.sharifl.edu>
2018-06-06 17:28:43 +0430

    complete memory.v and its testbench

--- memory.v
+++ memory.v
@@ -1,35 +1,28 @@
-module memory(clk,reset,address,data_in,data_out,rwn,start,ready);
+module memory(clk,reset,address,data_in,data_out,rwn,start);
 	input clk,reset,start,rwn;
 	input [15:0] address;
 	input [31:0] data_in;
 	output reg [31:0] data_out;
-	output ready;
 	reg [31:0] array[4097:0];
-	reg state;
+	reg state = 0;
 	reg [15:0] ad_t;
 	reg [31:0] data_t;
 	reg rwn_t;
 	integer i;
-	assign ready=~state;
-	always @(posedge clk or posedge reset)
-	begin
-		if(reset) begin
+	always @(posedge clk or reset)
+	begin	
+		if(~reset) begin
 			for(i=0; i<4098; i=i+1) begin
-				array[i] <= 8'h00;
+				array[i] <= 32'h00000000;
 			end
 			state=0;
 		end
-		else if(start & ~state) begin
+		else if(start) begin
 			ad_t=address[11:0];
-			rwn_t=rwn;
-			data_t=data_in;
-			state=1;
-		end
-		else if(state) begin
-			if(rwn_t)
+			if(rwn)
 				data_out = array[ad_t];
 			else begin
-				array[ad_t] <= data_t[31:0];
+				array[ad_t] <= data_in[31:0];
 			end
 			state=0;
 		end
---
+++ memory_tb.v
@@ -0,0 +1,99 @@
+module memory_tb;
+	reg [15:0] address;
+	reg [31:0] data_in;
+	reg start, clk, reset, rwn;
+	wire [31:0] data_out;
+	memory memory1(clk, reset, address, data_in, data_out, rwn, start);
+	initial begin
+		start = 1;
+		reset = 1;
+		rwn = 0;
+		clk = 0;
+	end
+	always #5 clk = !clk;
+	initial begin
+		#1
+		address = 16'h0000;
+		data_in = 32'h00000000;
+		#20
+		address = 16'h0001;
+		data_in = 32'h11111111;
+		#20
+		address = 16'h0002;
+		data_in = 32'h22222222;
+		#20
+		address = 16'h0003;
+		data_in = 32'h33333333;
+		#20
+		address = 16'h0004;
+		data_in = 32'h44444444;
+		#20
+		address = 16'h0005;
+		data_in = 32'h55555555;
+		#20
+		address = 16'h0006;
+		data_in = 32'h66666666;
+		#20
+		address = 16'h0007;
+		data_in = 32'h77777777;
+		#20
+		address = 16'h0008;
+		data_in = 32'h88888888;
+		#20
+		address = 16'h0009;
+		data_in = 32'h99999999;
+		#20
+		address = 16'h000A;
+		data_in = 32'hAAAAAAAA;
+		#20
+		address = 16'h000B;
+		data_in = 32'hBBBBBBBB;
+		#20
+		address = 16'h000C;
+		data_in = 32'hCCCCCCCC;
+		#20
+		address = 16'h000D;
+		data_in = 32'hDDDDDDDD;
+		#20
+		address = 16'h000E;
+		data_in = 32'hEEEEEEEE;
+		#20
+		address = 16'h000F;
+		data_in = 32'hFFFFFFFF;
+		#20
+		rwn = 1;
+		address = 16'h0000;
+		#20
+		address = 16'h0001;
+		#20
+		address = 16'h0002;
+		#20
+		address = 16'h0003;
+		#20
+		address = 16'h0004;
+		#20
+		address = 16'h0005;
+		#20
+		address = 16'h0006;
+		#20
+		address = 16'h0007;
+		#20
+		address = 16'h0008;
+		#20
+		address = 16'h0009;
+		#20
+		address = 16'h000A;
+		#20
+		address = 16'h000B;
+		#20
+		address = 16'h000C;
+		#20
+		address = 16'h000D;
+		#20
+		address = 16'h000E;
+		#20
+		address = 16'h000F;
+		#20
+		$stop;
+	end
+endmodule 
